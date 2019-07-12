0ea4109a63ae3cb60557194b2255ecf8cfc67c8c
火六 <1512751361@qq.com>
2019-07-12 20:09:27 +0800

    1. 新增手机屏幕计算

--- src/modules/service/index.jsx
+++ src/modules/service/index.jsx
@@ -1,9 +1,196 @@
 import React from 'react';
+import iphoneData from './phoneData';
 
-export default function () {
-	return (
-		<div>
+export default class Service extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			width: 375,
+			height: 667,
+			scale: 1,
+		};
+	}
+
+	render() {
+		const width = 750 / 2;
+		const height = 1334 / 2;
+		return (
+			<div>
       服务中心
-		</div>
-	);
+			UI：width:
+				{' '}
+				{width}
+				{' '}
+			height:
+				{' '}
+				{height}
+				<br />
+				{this.state.width * this.state.scale}
+				<br />
+				{this.state.height * this.state.scale}
+				<div style={{
+					backgroundColor: 'red',
+					width,
+					height,
+					transform: [
+						{
+							translateX: -this.state.width * 0.5,
+						},
+						{
+							translateY: -this.state.height * 0.5,
+						},
+						{
+							scale: this.state.scale,
+						},
+						{
+							translateX: this.state.width * 0.5,
+						},
+						{
+							translateY: this.state.height * 0.5,
+						},
+					],
+				}}
+				/>
+				<table cellPadding={10} cellSpacing={10}>
+					<tr>
+						<th>设备名称</th>
+						<th>操作系统</th>
+						<th>尺寸 in</th>
+						<th>PPI</th>
+						<th>纵横比</th>
+						<th>
+							{'宽 x 高 dp'}
+						</th>
+						<th>
+							{'宽 x 高 px'}
+						</th>
+						<th>密度 dpi</th>
+						<th>密度 type</th>
+						<th>fwDesignScale</th>
+						<th>fwWidth</th>
+						<th>fwHeight</th>
+						<th>fwScale</th>
+						<th />
+						<th>fhDesignScale</th>
+						<th>fhWidth</th>
+						<th>fhHeight</th>
+						<th>fhScale</th>
+						<th>操作</th>
+					</tr>
+					{
+						iphoneData.map((item) => {
+							const fwDesignScale = width / item.width;
+							const fwWidth = width;
+							let fwHeight = item.height * (width / item.width);
+							let fwScale = 1 / item.dpi / (width / item.width);
+
+							const fhDesignScale = height / item.height;
+							let fhWidth = item.width * (height / item.height);
+							const fhHeight = height;
+							const fhScale = 1 / item.dpi / (height / item.height);
+
+							let bg = '';
+							if (Math.round(fwWidth) === width && Math.round(fhWidth) === width) {
+								bg = 'green';
+							} else if ((Math.round(fwWidth) === width && fwScale < fhScale) || (Math.round(fhWidth) === width && fwScale > fhScale)) {
+								bg = 'red';
+							}
+							if (
+								fwHeight !== height
+							&& Math.round(fwHeight) === height
+							) {
+								fwHeight = Math.round(fwHeight);
+								fwScale = 1 / item.dpi / (fwHeight / item.height);
+							} else if (
+								fwHeight !== height
+							&& Math.floor(fwHeight) === height
+							) {
+								fwHeight = Math.floor(fwHeight);
+							} else if (
+								fwHeight !== height
+							&& Math.ceil(fwHeight) === height
+							) {
+								fwHeight = Math.ceil(fwHeight);
+							}
+
+							if (
+								fhWidth !== width
+							&& Math.round(fhWidth) === width
+							) {
+								fhWidth = Math.round(fhWidth);
+							} else if (
+								fhWidth !== width
+							&& Math.floor(fhWidth) === width
+							) {
+								fhWidth = Math.floor(fhWidth);
+							} else if (
+								fhWidth !== width
+							&& Math.ceil(fhWidth) === width
+							) {
+								fhWidth = Math.ceil(fhWidth);
+							}
+							return (
+								<tr style={{ backgroundColor: (item.isPad ? (bg ? 'blue' : 'pink') : bg) }}>
+									<td>{item.name}</td>
+									<td>{item.platform}</td>
+									<td>{item.in}</td>
+									<td>{item.PPI}</td>
+									<td>{item.scale}</td>
+									<td>
+										{item.widthDp}
+										{' X '}
+										{item.heightDp}
+									</td>
+									<td>
+										{item.width}
+										{' X '}
+										{item.height}
+									</td>
+									<td>{item.dpi}</td>
+									<td>{item.dpiType}</td>
+									<td>{fwDesignScale}</td>
+									<td>{fwWidth}</td>
+									<td>{fwHeight}</td>
+									<td>{fwScale}</td>
+									<td />
+									<td>{fhDesignScale}</td>
+									<td>{fhWidth}</td>
+									<td>{fhHeight}</td>
+									<td>{fhScale}</td>
+									<td>
+										<button
+											type="button"
+											onClick={() => {
+												this.setState({
+													width: fwWidth,
+													height: fwHeight,
+													scale: fwScale,
+												});
+											}}
+										>
+											{'执行1'}
+
+										</button>
+										<button
+											type="button"
+											onClick={() => {
+												this.setState({
+													width: fhWidth,
+													height: fhHeight,
+													scale: fhScale,
+												});
+											}}
+										>
+											{'执行2'}
+
+										</button>
+									</td>
+								</tr>
+							);
+						})
+					}
+				</table>
+			</div>
+		);
+	}
 }
---
+++ src/modules/service/phoneData.js
@@ -0,0 +1,176 @@
+export default [
+	{
+		name: 'iPhone X', platform: 'iOS', in: 5.8, PPI: 458, scale: '19 : 9', widthDp: 375, heightDp: 812, width: 1125, height: 2436, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'iPhone 8+ (8+, 7+, 6S+, 6+)', platform: 'iOS', in: 5.5, PPI: 401, scale: '16 : 9', widthDp: 414, heightDp: 736, width: 1242, height: 2208, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'iPhone 8 (8, 7, 6S, 6)', platform: 'iOS', in: 4.7, PPI: 326, scale: '16 : 9', widthDp: 375, heightDp: 667, width: 750, height: 1334, dpi: 2.0, dpiType: 'xhdpi',
+	},
+	{
+		name: 'iPhone SE（SE, 5S, 5C）', platform: 'iOS', in: 4.0, PPI: 326, scale: '16 : 9', widthDp: 320, heightDp: 568, width: 640, height: 1136, dpi: 2.0, dpiType: 'xhdpi',
+	},
+	{
+		name: 'iPhone 4, 4S', platform: 'iOS', in: 3.5, PPI: 326, scale: '16 : 9', widthDp: 320, heightDp: 480, width: 640, height: 960, dpi: 2.0, dpiType: 'xhdpi',
+	},
+	{
+		name: 'iPhone 1, 3G, 3GS', platform: 'iOS', in: 3.5, PPI: 163, scale: '16 : 9', widthDp: 320, heightDp: 480, width: 320, height: 480, dpi: 1.0, dpiType: 'hdpi',
+	},
+	{
+		name: 'iPad Pro 12.9', platform: 'iOS', in: 12.9, PPI: 264, scale: '4 : 3', widthDp: 1024, heightDp: 1336, width: 2048, height: 2732, dpi: 2.0, dpiType: 'xxxhdpi', isPad: true,
+	},
+	{
+		name: 'iPad Pro 10.5', platform: 'iOS', in: 10.5, PPI: 264, scale: '4 : 3', widthDp: 834, heightDp: 1112, width: 1668, height: 2224, dpi: 2.0, dpiType: 'xhdpi', isPad: true,
+	},
+	{
+		name: 'iPad Pro 9.7, iPad Air 2, Retina iPad', platform: 'iOS', in: 9.7, PPI: 264, scale: '4 : 3', widthDp: 768, heightDp: 1024, width: 1536, height: 2048, dpi: 2.0, dpiType: 'xhdpi', isPad: true,
+	},
+	{
+		name: 'iPad Mini 4, iPad Mini 2', platform: 'iOS', in: 7.9, PPI: 326, scale: '4 : 3', widthDp: 768, heightDp: 1024, width: 1536, height: 2048, dpi: 2.0, dpiType: 'xhdpi', isPad: true,
+	},
+	{
+		name: 'iPad 1, 2', platform: 'iOS', in: 9.7, PPI: 326, scale: '16 : 9', widthDp: 768, heightDp: 1024, width: 1536, height: 2048, dpi: 2.0, dpiType: 'xxxhdpi', isPad: true,
+	},
+	{
+		name: 'Android One', platform: 'Android', in: 4.5, PPI: 218, scale: '16 : 9', widthDp: 320, heightDp: 569, width: 480, height: 854, dpi: 1.5, dpiType: 'hdpi',
+	},
+	{
+		name: 'Google Pixel', platform: 'Android', in: 5.0, PPI: 441, scale: '16 : 9', widthDp: 411, heightDp: 731, width: 1080, height: 1920, dpi: 2.6, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Google Pixel XL', platform: 'Android', in: 5.5, PPI: 534, scale: '16 : 9', widthDp: 411, heightDp: 731, width: 1440, height: 2560, dpi: 3.5, dpiType: 'xxxhdpi',
+	},
+	{
+		name: 'Moto X', platform: 'Android', in: 4.7, PPI: 312, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 720, height: 1280, dpi: 2.0, dpiType: 'xhdpi',
+	},
+	{
+		name: 'Moto X 二代', platform: 'Android', in: 5.2, PPI: 424, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Nexus 5', platform: 'Android', in: 5.0, PPI: 445, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Nexus 5X', platform: 'Android', in: 5.2, PPI: 565, scale: '16 : 9', widthDp: 411, heightDp: 731, width: 1080, height: 1920, dpi: 2.6, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Nexus 6', platform: 'Android', in: 6.0, PPI: 493, scale: '16 : 9', widthDp: 411, heightDp: 731, width: 1440, height: 2560, dpi: 3.5, dpiType: 'xxxhdpi',
+	},
+	{
+		name: 'Nexus 6P', platform: 'Android', in: 5.7, PPI: 518, scale: '16 : 9', widthDp: 411, heightDp: 731, width: 1440, height: 2560, dpi: 3.5, dpiType: 'xxxhdpi',
+	},
+	{
+		name: 'Nexus 6P', platform: 'Android', in: 5.7, PPI: 518, scale: '16 : 9', widthDp: 411, heightDp: 731, width: 1440, height: 2560, dpi: 3.5, dpiType: 'xxxhdpi',
+	},
+	{
+		name: 'Samsung Galaxy S8', platform: 'Android', in: 5.8, PPI: 570, scale: '18.5 : 9', widthDp: 360, heightDp: 740, width: 1440, height: 2960, dpi: 4.0, dpiType: 'xxxhdpi',
+	},
+	{
+		name: 'Samsung Galaxy S8+', platform: 'Android', in: 6.2, PPI: 529, scale: '18.5 : 9', widthDp: 360, heightDp: 740, width: 1440, height: 2960, dpi: 4.0, dpiType: 'xxxhdpi',
+	},
+	{
+		name: 'Samsung Galaxy Note 4', platform: 'Android', in: 5.7, PPI: 515, scale: '16 : 9', widthDp: 480, heightDp: 853, width: 1440, height: 2560, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Samsung Galaxy Note 5', platform: 'Android', in: 5.7, PPI: 518, scale: '16 : 9', widthDp: 480, heightDp: 853, width: 1440, height: 2560, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Samsung Galaxy S5', platform: 'Android', in: 5.1, PPI: 432, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Samsung Galaxy S7 (S7, S6, S6 Edge)', platform: 'Android', in: 5.1, PPI: 576, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1440, height: 2560, dpi: 4.0, dpiType: 'xxxhdpi',
+	},
+	{
+		name: 'Samsung Galaxy S7 Edge', platform: 'Android', in: 5.5, PPI: 534, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1440, height: 2560, dpi: 4.0, dpiType: 'xxxhdpi',
+	},
+	{
+		name: 'Smartisan T2', platform: 'Android', in: 4.95, PPI: 445, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Smartisan M1', platform: 'Android', in: 5.15, PPI: 428, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Smartisan M1L', platform: 'Android', in: 5.7, PPI: 515, scale: '16 : 9', widthDp: 480, heightDp: 853, width: 1440, height: 2560, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: '坚果 Pro', platform: 'Android', in: 5.5, PPI: 403, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'OnePlus 5', platform: 'Android', in: 5.5, PPI: 401, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'OnePlus 3T', platform: 'Android', in: 5.5, PPI: 401, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Oppo R9s (R9s, R11)', platform: 'Android', in: 5.5, PPI: 401, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Oppo R9s Plus (R9s Plus, R11 Plus)', platform: 'Android', in: 6.0, PPI: 368, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Oppo A57', platform: 'Android', in: 5.2, PPI: 282, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 720, height: 1280, dpi: 2.0, dpiType: 'xhdpi',
+	},
+	{
+		name: 'Oppo A59s', platform: 'Android', in: 5.5, PPI: 267, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 720, height: 1280, dpi: 2.0, dpiType: 'xhdpi',
+	},
+	{
+		name: 'Oppo A37', platform: 'Android', in: 5.0, PPI: 293, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 720, height: 1280, dpi: 2.0, dpiType: 'xhdpi',
+	},
+	{
+		name: '小米MIX2', platform: 'Android', in: 5.99, PPI: 403, scale: '18 : 9', widthDp: 360, heightDp: 720, width: 1080, height: 2160, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: '小米MIX', platform: 'Android', in: 6.4, PPI: 362, scale: '17 : 9', widthDp: 360, heightDp: 680, width: 1080, height: 2040, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: '小米Note 3', platform: 'Android', in: 5.5, PPI: 403, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: '小米Note 2', platform: 'Android', in: 5.7, PPI: 386, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: '小米6', platform: 'Android', in: 5.15, PPI: 428, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: '小米5s', platform: 'Android', in: 5.15, PPI: 428, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: '小米5s Plus', platform: 'Android', in: 5.7, PPI: 386, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: '小米Max', platform: 'Android', in: 6.44, PPI: 342, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: '红米Note 4 (4, Note 4X)', platform: 'Android', in: 5.5, PPI: 403, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: '红米 4 (4, 4X)', platform: 'Android', in: 5.0, PPI: 296, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 720, height: 1280, dpi: 2.0, dpiType: 'xhdpi',
+	},
+	{
+		name: 'Vivo X9 (X9, X9s)', platform: 'Android', in: 5.5, PPI: 401, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Vivo X9 Plus (X9 Plus, X9s Plus)', platform: 'Android', in: 5.88, PPI: 375, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'HUAWEI P10', platform: 'Android', in: 5.1, PPI: 432, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1080, height: 1920, dpi: 3.0, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'HUAWEI P10 Plus', platform: 'Android', in: 5.5, PPI: 540, scale: '16 : 9', widthDp: 360, heightDp: 640, width: 1440, height: 2560, dpi: 4.0, dpiType: 'xxxhdpi',
+	},
+	{
+		name: '小米测试机', platform: 'Android', in: 5.5, PPI: 540, scale: '16 : 9', widthDp: 392.727272, heightDp: 698.181818, width: 1080, height: 1920, dpi: 2.75, dpiType: 'xxhdpi',
+	},
+	{
+		name: 'Google Pixel C', platform: 'Android', in: 10.2, PPI: 308, scale: '4 : 3', widthDp: 900, heightDp: 1280, width: 1800, height: 2560, dpi: 2.0, dpiType: 'xhdpi', isPad: true,
+	},
+	{
+		name: 'Nexus 9', platform: 'Android', in: 8.9, PPI: 288, scale: '4 : 3', widthDp: 768, heightDp: 1024, width: 1536, height: 2048, dpi: 2.0, dpiType: 'xhdpi', isPad: true,
+	},
+	{
+		name: 'Surface 3', platform: 'Windows', in: 10.8, PPI: 214, scale: '16 : 9', widthDp: 720, heightDp: 1080, width: 1080, height: 1920, dpi: 1.5, dpiType: 'hdpi', isPad: true,
+	},
+	{
+		name: '小米平板 2', platform: 'Android', in: 7.9, PPI: 326, scale: '16 : 9', widthDp: 768, heightDp: 1024, width: 1536, height: 2048, dpi: 2.0, dpiType: 'xhdpi', isPad: true,
+	},
+];
